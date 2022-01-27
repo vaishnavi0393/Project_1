88a380d3412a3c5db3bc80a51897f4983ab8a26e
vaishnavi0393 <vbhoye90@gmail.com>
2022-01-27 19:05:24 +0530

    form validation

--- somejs.js
+++ somejs.js
@@ -1,10 +1,19 @@
+
+
 $("#lselect").click(function(){
     $("#signup-div").hide();
+    $("[name='l_form']").trigger("reset");
+    document.getElementsByClassName("email_msg")[1].innerHTML="";
+    document.getElementsByClassName("psw_msg")[1].innerHTML="";
     $("#login-div").show();
 });
 
 $("#sselect").click(function(){
     $("#login-div").hide();
+    $("[name='s_form']").trigger("reset");
+    document.getElementsByClassName("uname_msg")[0].innerHTML="";
+    document.getElementsByClassName("email_msg")[0].innerHTML="";
+    document.getElementsByClassName("psw_msg")[0].innerHTML="";
     $("#signup-div").show();
 });
 
@@ -25,3 +34,47 @@ $(document).ready(function(){
         }
     });
 });
+
+$("#signup-div").submit(function(e) {
+    e.preventDefault();
+});
+
+$("#login-div").submit(function(e) {
+    e.preventDefault();
+});
+
+
+function validateForm(f_name) {
+    var i;
+    if (f_name=="s_form")
+    {
+        var uname = document.forms["s_form"]["uname"].value;
+        var email = document.forms["s_form"]["new_email"].value;
+        var psw = document.forms["s_form"]["new_password"].value;
+        var only_spaces = uname.match(/[ \n\r\t\f]+/g).toString().length;
+        var test1 = uname.match(/\W/g).toString()!=uname.match(/\s/g).toString();
+        i=0;
+    }
+    
+    if (f_name=="l_form"){
+        var email = document.forms["l_form"]["email"].value;
+        var psw = document.forms["l_form"]["password"].value;
+        i=1;
+    }
+    let check_email = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/g.test(email);
+    let test2 = /[ \n\r\t\f]+/g.test(psw);
+    if (f_name=="s_form") {
+        if (/\d/g.test(uname)==true || test1 || uname.length==only_spaces)
+        {
+            document.getElementsByClassName("uname_msg")[i].innerHTML="Please enter valid name";
+        }
+    }
+    if (check_email==false)
+    {
+        document.getElementsByClassName("email_msg")[i].innerHTML="Please Enter valid Email Id";
+    }
+    if (test2)
+    {
+        document.getElementsByClassName("psw_msg")[i].innerHTML="Please Enter valid Password";
+    }
+}
